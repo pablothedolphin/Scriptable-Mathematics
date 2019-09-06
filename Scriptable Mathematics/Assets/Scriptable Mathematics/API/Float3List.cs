b9d72598c8fb41b40624f062d21d1d406cda79c6
Jak <jak.hussain@arup.com>
2019-09-06 13:59:58 +0100

    First commit.

---
+++ Scriptable Mathematics/Assets/Scriptable Mathematics/API/Float3List.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+namespace ScriptableFramework.Mathematics
+{
+	/// <summary>
+	/// A <c>ValueList</c> which can hold a list of float3s.
+	/// </summary>
+	[CreateAssetMenu (menuName = "Runtime Objects/Value/List/Float3s")]
+	public class Float3List : ValueList<float3>
+	{
+
+	}
+}
---
+++ Scriptable Mathematics/Assets/Scriptable Mathematics/API/Float3Value.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+namespace ScriptableFramework.Mathematics
+{
+	/// <summary>
+	/// A <c>ValueItem</c> which can hold a float3.
+	/// </summary>
+	[CreateAssetMenu (menuName = "Runtime Objects/Value/Item/Float3")]
+	public class Float3Value : ValueItem<float3>
+	{
+
+	}
+}
