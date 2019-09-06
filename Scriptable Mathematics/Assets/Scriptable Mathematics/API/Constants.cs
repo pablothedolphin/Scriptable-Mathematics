f5ae2eb97db535aa44adac8e3521549186351e49
Jakir Hussain <jakir.hussain@hotmail.co.uk>
2019-09-06 23:13:36 +0100

    0.1.4: Added a static const for storing the golden ratio phi as both a double and a float since the unity math library is missing it.

---
+++ Scriptable Mathematics/Assets/Scriptable Mathematics/API/Constants.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptableFramework.Mathematics
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class Constants
+	{
+		/// <summary>The mathematical constant phi. Approximately 1.62. This is a f64/double precision constant.</summary>
+		public const double PHI_DBL = 1.61803398874989484820;
+
+		/// <summary>The mathematical constant phi. Approximately 1.62.</summary>
+		public const float PHI = (float)PHI_DBL;
+	}
+}
